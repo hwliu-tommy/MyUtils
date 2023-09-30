f422d33b23c9a3b4349e6f1b62e7f3e827fedd00
Tommy Liu <hwliu.tommy@gmail.com>
2023-09-30 20:55:19 +0800

    Support kwargs for NetworkUtils.

--- MyUtils/NetworkUtils.py
+++ MyUtils/NetworkUtils.py
@@ -1,24 +1,26 @@
 
 import requests
 
-def get_html(uri):
-	resp = requests.get(uri)
+# Functions
+def get_html(uri, **kwargs):
+        resp = requests.get(uri, kwargs)
 
-	if not resp.ok:
-		raise RuntimeError('Fail to get HTML from uri={}\nError code = {}\nResp text = {}'.format(
-			uri, resp.status_code, resp.text))
+        if not resp.ok:
+                raise RuntimeError('Fail to get HTML from uri={}\nError code = {}\nResp text = {}'.format(
+                        uri, resp.status_code, resp.text))
 
-	return resp.text
+        return resp.text
 
-def get_bin(fname, uri, dry_run=False):
-	if dry_run:
-		print("Download binary file: uri={}, fname={}".format(uri, fname))
-	else:
-		r = requests.get(uri, stream=True)
+def get_bin(fname, uri, dry_run=False, **kwargs):
+        if dry_run:
+                print("Download binary file: uri={}, fname={}, kwargs={}".format(uri, fname, kwargs))
+        else:
+                r = requests.get(uri, kwargs, stream=True)
 
-		if r.status_code == 200:
-			with open(fname, 'wb') as f:
-				for chunk in r.iter_content(1024):
-					f.write(chunk)
+                if r.status_code == 200:
+                        with open(fname, 'wb') as f:
+                                for chunk in r.iter_content(1024):
+                                        f.write(chunk)
+        return fname
 
 	return fname
